3554356ee4f8b45509e127f5ab9a8617a5913ef1
Mateo MARTINEZ <mateo.martinez@etu.univ-rouen.fr>
2022-03-15 15:37:30 +0100

    Improve BottomBar

--- lib/view/home_page/components/bottom_bar.dart
+++ lib/view/home_page/components/bottom_bar.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class BottomBar extends StatelessWidget {
+class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return BottomNavigationBar(items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.abc), label:"abc"),
-      BottomNavigationBarItem(icon: Icon(Icons.abc), label:"abc"),
-    ]);
+  State<StatefulWidget> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  int get selectedIndex {return _selectedIndex;}
+
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
   }
 
-  
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+        BottomNavigationBarItem(icon: Icon(Icons.explore_outlined), label: "explore"),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: "more"),
+        BottomNavigationBarItem(icon: Icon(Icons.messenger_outline), label: "messages"),
+        BottomNavigationBarItem(icon: Icon(Icons.notifications_outlined), label: "notifications"),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
 }
